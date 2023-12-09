66675869d16c8d30cdfc5face57178ea3b724946
adham adel <adhamadel198@gmail.com>
2023-12-09 18:37:31 +0200

    updated version

--- src/UI/navbar/navbar.js
+++ src/UI/navbar/navbar.js
@@ -6,11 +6,9 @@ const Navbar = () =>{
         <nav className="">
           <ul className="flex justify-center items-center space-x-2 p-4">
             <Navitem to="/home">Home</Navitem>
-            <Navitem to="/articles">Articles</Navitem>
-            <Navitem to="/articles/add">Add Articles</Navitem>
+            <Navitem to="/articles">All Articles</Navitem>
             <Navitem to="/">Sign In</Navitem>
             <Navitem to="/register">Sign Up</Navitem>
-            <Navitem to="/drafts">drafts</Navitem>
             <SearchBar/>
           </ul>
         </nav>
