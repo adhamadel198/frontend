1a5bf58b1afd2960a0dd3217f21aaee1a56692a9
adham adel <adhamadel198@gmail.com>
2023-12-09 22:35:22 +0200

    updated version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddArticlePage from './pages/AddArticlePage';
 import Articlespage from './pages/ArticlesPage';
+import ArticlesPageAdmin from './pages/ArticlesPageAdmin';
 import HomePage from './pages/homePage.js';
+import journalistPage from './pages/journalistPage.js';
+import ShowAllJournalist from './pages/ShowAllJournalist.js';
+import AdminPage from './pages/AdminPage.js';
 import SearchPage from './pages/SearchPage';
 import Layout from './components/Layout';
+import LayoutJournalist from './components/LayoutJournalist.js';
+import LayoutAdmin from './components/LayoutAdmin.js';
 import DraftsPage from './pages/drafts';
 import UpdateDraftPage from './pages/UpdateDraftPage';
 import Register from "./pages/Register.js";
@@ -22,11 +28,15 @@ const App = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/topic" element={<Topic />} />
         <Route path="/home" element={<Layout><HomePage /></Layout>}/>
-        <Route path="/articles"element={<Layout><Articlespage /></Layout>}/>
-        <Route path="/articles/add"element={<Layout><AddArticlePage /></Layout>}/>
+        <Route path="/journalistPage" element={<LayoutJournalist><journalistPage /></LayoutJournalist>}/>
+        <Route path="/articles"element={<LayoutJournalist><Articlespage /></LayoutJournalist>}/>
+        <Route path="/articles/add"element={<LayoutJournalist><AddArticlePage /></LayoutJournalist>}/>
+        <Route path="/drafts"element={<LayoutJournalist><DraftsPage /></LayoutJournalist>}/>
+        <Route path="/updatedraftpage/:id"element={<LayoutJournalist><UpdateDraftPage /></LayoutJournalist>}/>
+        <Route path="/AdminPage" element={<LayoutAdmin><AdminPage /></LayoutAdmin>}/>
+        <Route path="/ArticlesPageAdmin"element={<LayoutAdmin><ArticlesPageAdmin /></LayoutAdmin>}/>
+        <Route path="/publisher"element={<LayoutAdmin><ShowAllJournalist /></LayoutAdmin>}/>
         <Route path="/search-results/:searchTerm"element={<Layout><SearchPage /></Layout>}/>
-        <Route path="/drafts"element={<Layout><DraftsPage /></Layout>}/>
-        <Route path="/updatedraftpage/:id"element={<Layout><UpdateDraftPage /></Layout>}/>
       </Routes>
     </BrowserRouter>
   );
